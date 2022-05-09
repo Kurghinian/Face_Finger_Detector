7fb216154b89bab8af6bf1fdd917d3da27e96dca
Khachh <84278266+Kurghinian@users.noreply.github.com>
2022-05-09 16:41:26 +0400

    Delete pyimage.py

--- pyimage.py
+++
@@ -1,8 +0,0 @@
-import cv2
-import numpy
-
-image = cv2.imread('balloon.jpg')
-cv2.putText(image, "ջվգե", ( 200, 100), 0, 3, (255, 255,255), 5)
-cv2.imshow("image",image)
-cv2.waitKey(0) 
-cv2.destroyAllWindows()
